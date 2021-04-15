2de69f9a49fa1c472ccc7c5baec4951096409cf3
Windact <52325204+Windact@users.noreply.github.com>
2021-04-15 06:02:42 +0200

    THe app has been containerized and the port has been put to 8888 as requiered

--- run.py
+++ run.py
@@ -11,5 +11,5 @@ app = Flask(__name__)
 if __name__ == '__main__':
     logger.debug("Starting the application")
     from api import *
-    app.run(host="0.0.0.0",port=5000, debug=True, use_reloader=True)
+    app.run(host="0.0.0.0",port=8888, debug=False)
 
